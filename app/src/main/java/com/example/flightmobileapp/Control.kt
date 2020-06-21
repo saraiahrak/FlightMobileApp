9a709c42608f99658116acbaea64d6fcf0e2044e
saraiahrak <saraiahrak99@gmail.com>
2020-06-21 17:33:41 +0300

    lanscape orientation

--- app/src/main/java/com/example/flightmobileapp/Control.kt
+++ app/src/main/java/com/example/flightmobileapp/Control.kt
@@ -1,25 +1,29 @@
 package com.example.flightmobileapp
 
+import android.content.ClipData
+import android.database.Cursor
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
 import android.widget.Button
 import android.widget.SeekBar
 import android.widget.TextView
 import androidx.annotation.RequiresApi
 import kotlin.math.round
-import kotlinx.android.synthetic.main.activity_control.*
+
 
 class Control : AppCompatActivity() {
 
-    lateinit var rudderSeekbar: SeekBar
-    lateinit var throttleSeekbar: SeekBar
-    lateinit var rudder: TextView
-    lateinit var aileron: TextView
-    lateinit var elevator: TextView
-    lateinit var throttle: TextView
-    lateinit var knob: Button
-    lateinit var knobLimits: Button
+    private lateinit var rudderSeekbar: SeekBar
+    private lateinit var throttleSeekbar: SeekBar
+    private lateinit var rudder: TextView
+    private lateinit var aileron: TextView
+    private lateinit var elevator: TextView
+    private lateinit var throttle: TextView
+    private lateinit var knob: Button
+    private lateinit var knobLimits: Button
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,17 +42,28 @@ class Control : AppCompatActivity() {
         throttle = findViewById(R.id.throttle)
         knob = findViewById(R.id.knob)
         knobLimits = findViewById(R.id.background)
-        rudder.text = "0"
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
     private fun initListeners() {
         initSeekBars()
+        initKnob()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun initKnob() {
+        knob.isLongClickable = true
+
+        knob.setOnLongClickListener(object : View.OnLongClickListener {
+            override fun onLongClick(p0: View?): Boolean {
+                return true
+            }
+
+        })
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
     private fun initSeekBars() {
-
         initRudderBar()
         initThrottleBar()
     }
@@ -62,8 +77,8 @@ class Control : AppCompatActivity() {
             return round(this * multiplier) / multiplier
         }
 
-        var min = -10
-        var max = 10
+        val min = -10
+        val max = 10
         rudderSeekbar.max = max
         rudderSeekbar.min = min
 
@@ -73,8 +88,8 @@ class Control : AppCompatActivity() {
                     p1.toDouble(),
                     (-1).toDouble(),
                     1.toDouble(),
-                    (-10).toDouble(),
-                    10.toDouble()
+                    min.toDouble(),
+                    max.toDouble()
                 )
                 rudder.text = progress.round(2).toString()
             }
@@ -92,19 +107,19 @@ class Control : AppCompatActivity() {
 
     @RequiresApi(Build.VERSION_CODES.O)
     private fun initThrottleBar() {
-        var min = 0
-        var max = 10
+        val min = 0
+        val max = 10
         throttleSeekbar.max = max
         throttleSeekbar.min = min
 
         throttleSeekbar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
-                var progress = Utils.normalize(
+                val progress = Utils.normalize(
                     p1.toDouble(),
                     (0).toDouble(),
                     1.toDouble(),
-                    (0).toDouble(),
-                    10.toDouble()
+                    min.toDouble(),
+                    max.toDouble()
                 )
                 throttle.text = progress.toString()
             }
